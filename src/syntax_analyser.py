617d7eadd910ac5028d1a1223b8a08581357abc0
Leila Dardouri <leila.dardouri@outlook.it>
2023-11-07 12:22:34 +0100

    check well-formatted loops

--- src/syntax_analyser.py
+++ src/syntax_analyser.py
@@ -1,22 +1,17 @@
-import re
-
+import analyzer
 
 class SyntaxAnalyzer:
     def __init__(self):
         self.runtime = 0
 
     def analyze(self, code):
-        is_open_bracket = re.search('[', code)
-        is_closed_bracket = re.search(']', code)
-
-        print(is_closed_bracket, is_open_bracket)
+        is_open_bracket = code.find('[')
+        is_closed_bracket = code.find(']')
 
         if is_open_bracket is None and is_closed_bracket is None:
             return True
-        elif (is_open_bracket is None and is_closed_bracket is not None) or (
-            is_open_bracket is not None and is_closed_bracket is None):
-            raise SyntaxError()
-        else: # if there is a loop assume that the program never halts
+        else: # if there are brackets check if loops are well-formatted, if they are assume that program never halts
+            analyzer.not_well_formatted_loop(code)
             return False
 
         
