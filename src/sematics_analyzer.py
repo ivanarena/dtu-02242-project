b903c22db7b3ef8bd7cd5db8975d3f0803655bda
ivanarena <ivan.x28x@gmail.com>
2023-11-13 15:45:24 +0100

    Detect nested loop (not consecutive)

--- src/sematics_analyzer.py
+++ src/sematics_analyzer.py
@@ -1,82 +1,90 @@
 import re
 import time
+import pprint
+UPPER_BOUND = 30000
+LOWER_BOUND = 0
 
 class SemanticsAnalyzer:
     def __init__(self):
         self.runtime = 0
         self.ptr = 0
 
-    def _parse_loops(self, code):
-        """
-        Returns loops and variables abstractions dicts
-        
-        Inputs:
-            - code: Brainfuck program
-        
-        Outputs:
-            - loops: dict: key: memory cell index, value: array of loop objects 
-                                (e.g.) {
-                                            'start': loop start index,
-                                            'end': loop end index,
-                                            'code': loop code,
-                                            'depth': loop depth,
-                                        }
-            - variables: dict: key: memory cell index, value: memory cell value 
-        """
+class SemanticsAnalyzer:
+    def __init__(self):
+        self.runtime = 0
+
+    def _find_loop_end(self, code, start):
+        counter = 1
+        for i in range(start + 1, len(code)):
+            if code[i] == '[':
+                counter += 1
+            elif code[i] == ']':
+                counter -= 1
+            if counter == 0:
+                return i
+        raise SyntaxError(f"No matching closing bracket for the opening bracket at index {start}")
+
+    def _parse_loops(self, code, start=0, end=None, ptr=0, curr_var=0, variables = {}):
         stack = []
-        ptr_stack = []
-        loops = {}
-        variables = {}
-        depth = -1
-        
-        curr_var = 0
-        for i, ch in enumerate(code):
-            # TODO: implement and abstract input command
+
+        if end is None:
+            end = len(code)
+
+        for i in range(start, end):
+            ch = code[i]
+            print(ch, ptr)
             if ch == '>':
-                self.ptr += 1
-                if self.ptr in variables.keys():
-                    curr_var = variables[self.ptr]
+                if (ptr == UPPER_BOUND): # upper bound err
+                    raise SystemError('Pointer out of bounds')
+                ptr += 1
+                if ptr in variables.keys():
+                    curr_var = variables[ptr]
                 else:
                     curr_var = 0
             elif ch == '<':
-                self.ptr -= 1
-                if self.ptr in variables.keys():
-                    curr_var = variables[self.ptr]
+                if (ptr == LOWER_BOUND): # lower bound err
+                    raise SystemError('Pointer out of bounds')
+                ptr -= 1
+                if ptr in variables.keys():
+                    curr_var = variables[ptr]
                 else:
                     curr_var = 0
             elif ch == '+':
-                if (self.ptr < 0):
-                    raise SystemError('Pointer out of bounds') #TODO add upper bound to memory tape
                 curr_var += 1
-                variables[self.ptr] = curr_var 
+                variables[ptr] = curr_var 
             elif ch == '-':
-                if (self.ptr < 0):
-                    raise SystemError('Pointer out of bounds')
                 curr_var -= 1
-                variables[self.ptr] = curr_var 
+                variables[ptr] = curr_var 
             elif ch == '[':
-                ptr_stack.append(self.ptr)
-                stack.append(i)
-                depth += 1
+                loop = {
+                    "cell": ptr,
+                    "value": curr_var,
+                    "start": i,
+                    "code": "",
+                    "has_nested_loop": False,
+                    "nested_loop": []
+                }
+                if stack:
+                    stack[-1]["has_nested_loop"] = True
+                stack.append(loop)
             elif ch == ']':
                 if not stack:
-                    raise SyntaxError(f"Unbalanced brackets at index {i}")
-                start = stack.pop()
-                if ptr_stack[-1] not in loops.keys():
-                    loops[ptr_stack[-1]] = []
-                loops[ptr_stack[-1]].append({
-                    "start": start,
-                    "end": i,
-                    "code": code[start+1:i],
-                    "depth": depth
-                })
-                ptr_stack.pop()
-                depth -= 1
-        if stack:
-            raise SyntaxError(f"Unbalanced brackets at index {stack[0]}")
-        
+                    raise SyntaxError('Loop end found without a corresponding loop start')
+                loop = stack.pop()
+                loop["end"] = i
+                loop["code"] = code[loop["start"]+1:i]
+                if stack:
+                    stack[-1]["nested_loop"].append(loop)
+                else:
+                    return loop, variables
 
-        return loops, variables
+        if stack:
+            raise SyntaxError('Loop start found without a corresponding loop end')
+        return stack, variables
+
+
+
+
 
 
     def analyze(self, code):
@@ -92,73 +100,14 @@ class SemanticsAnalyzer:
                 level: 0
             }
         """
-        loops, vars = self._parse_loops(code)      
-        print("CODE", code)
-        print("LOOPS", loops)
-        print("VARS", vars)
+        loops, variables = self._parse_loops(code)      
+        print("CODE")
+        pprint.pprint(code)
+        print("LOOPS")
+        pprint.pprint(loops)
+        print("variables")
+        pprint.pprint(variables)
         
-        updated_vars = vars
-        for key, elem in loops.items(): # iterate through the loops in the program
-            initial_ptr = key
-            current_ptr = initial_ptr
-            #vars values are the ones post first iteration of loops
-            if vars[initial_ptr] == 0: # just one iteration to make the entering variable reach zero
-                print(f'loop {key} halts')
-                continue # move on to outer loop
-
-            for loop in elem:
-                # here it holds that vars[initial_ptr] != 0
-                print("LOOP", loop['code'])
-
-                if not loop['code']: # empty loop
-                    raise SystemError('Not halting: loop is empty')
-                 
-                if re.search(r'^[^+-]*$', loop['code']) : # no + - inside loop (e.g., [>>>>] wont halt) [not complete because >>>[>+>>] wont match]
-                    raise SystemError('Not halting: loop has no writing operations')
-                
-                if loop['code'].count('+') > 0 and re.search(r'^[^-<>]*$', loop['code']): # (not considering input for now) if a loop has no - or moving operations
-                    if (loop['code'].count('+') % 2 == 0 and vars[initial_ptr] % 2 == 0):
-                        print('it halts')
-                        vars[initial_ptr] = 0
-                        continue
-                    elif (loop['code'].count('+') % 2 == 0 and vars[initial_ptr] % 2 != 0):
-                        raise SystemError('Not halting: odd inital value and even increment') # does this always hold?
-                    elif (loop['code'].count('+') % 2 != 0 and vars[initial_ptr] % 2 == 0):
-                        raise SystemError('Not halting: even inital value and odd increment') # does this always hold?
-                    
-
-                if loop['code'].count('-') > 0 and re.search(r'^[^+<>]*$', loop['code']): # (not considering input for now) if a loop has no + or moving operations
-                    if (loop['code'].count('-') % 2 == 0 and vars[initial_ptr] % 2 == 0):
-                        print('it halts')
-                        vars[initial_ptr] = 0
-                        continue
-                    elif (loop['code'].count('+') % 2 == 0 and vars[initial_ptr] % 2 != 0):
-                        raise SystemError('Not halting: odd inital value and even increment')                 
-
-
-                for cmd in loop['code']: # to evaluate vars after another loop iteration (the 2nd one) 
-                    print('currptr', current_ptr)
-                    print('cmd', cmd)
-                    if cmd == '+':
-                        updated_vars[current_ptr] = (updated_vars[current_ptr] + 1) % 256
-                    elif cmd == '-':
-                        updated_vars[current_ptr] = (updated_vars[current_ptr] - 1) % 256
-                    elif cmd == '>':
-                        current_ptr += 1
-                    elif cmd == '<':
-                        current_ptr -= 1
-
-                print("UPDATED VARS", updated_vars)
-                if updated_vars[initial_ptr] == 0:
-                    print('it halts')
-                # elif updated_vars[initial_ptr] < vars[initial_ptr] and ((vars[initial_ptr] - updated_vars[initial_ptr] % 2 != 0 and updated_vars[initial_ptr] % 2 != 0) or (vars[initial_ptr] - updated_vars[initial_ptr] % 2 == 0 and updated_vars[initial_ptr] % 2 == 0)):
-                #     print('halts')
-                else:
-                    raise SystemError('not halts') # give up and say that it does not halt (?)
-                    
-            vars[initial_ptr] = 0 # update vars with the initial of this cycle (or with all the changed values?)
-                                
-
         # TODO:
         # 1.    decide queue order
         # 2.    for every loop in order:
@@ -168,11 +117,76 @@ class SemanticsAnalyzer:
         #       2.3.1a   if loop terminates continue
         #       2.3.1b   if loop doesn't terminate raise exception 
                             # loop for sure does not terminate if the variable I entered the loop with stays untouched
+        """
+        updated_variables = variables
+        for key, elem in loops.items(): # iterate through the loops in the program
+            initial_ptr = key
+            current_ptr = initial_ptr
+
+            #variables values are the ones post first iteration of loops
+            if variables[initial_ptr] == 0: # just one iteration to make the entering variable reach zero
+                print(f'loop {key} halts')
+                continue # move on to outer loop
+
+            for loop in elem:
+                # here it holds that variables[initial_ptr] != 0
+                print("LOOP", loop['code'])
+
+                if not loop['code']: # empty loop
+                    raise SystemError('Not halting: loop is empty')
+                 
+                if re.search(r'^[^+-]*$', loop['code']) : # no + - inside loop (e.g., [>>>>] wont halt) [not complete because >>>[>+>>] wont match]
+                    raise SystemError('Not halting: loop has no writing operations')
+                
+                if loop['code'].count('+') > 0 and re.search(r'^[^-<>]*$', loop['code']): # (not considering input for now) if a loop has no - or moving operations
+                    if (loop['code'].count('+') % 2 == 0 and variables[initial_ptr] % 2 == 0):
+                        print('it halts')
+                        variables[initial_ptr] = 0
+                        continue
+                    elif (loop['code'].count('+') % 2 == 0 and variables[initial_ptr] % 2 != 0):
+                        raise SystemError('Not halting: odd inital value and even increment') # does this always hold?
+                    elif (loop['code'].count('+') % 2 != 0 and variables[initial_ptr] % 2 == 0):
+                        raise SystemError('Not halting: even inital value and odd increment') # does this always hold?
+                    
+
+                if loop['code'].count('-') > 0 and re.search(r'^[^+<>]*$', loop['code']): # (not considering input for now) if a loop has no + or moving operations
+                    if (loop['code'].count('-') % 2 == 0 and variables[initial_ptr] % 2 == 0):
+                        print('it halts')
+                        variables[initial_ptr] = 0
+                        continue
+                    elif (loop['code'].count('+') % 2 == 0 and variables[initial_ptr] % 2 != 0):
+                        raise SystemError('Not halting: odd inital value and even increment')                 
+
+
+                for cmd in loop['code']: # to evaluate variables after another loop iteration (the 2nd one) 
+                    print('currptr', current_ptr)
+                    print('cmd', cmd)
+                    if cmd == '+':
+                        updated_variables[current_ptr] = (updated_variables[current_ptr] + 1) % 256
+                    elif cmd == '-':
+                        updated_variables[current_ptr] = (updated_variables[current_ptr] - 1) % 256
+                    elif cmd == '>':
+                        current_ptr += 1
+                    elif cmd == '<':
+                        current_ptr -= 1
+
+                print("UPDATED variables", updated_variables)
+                if updated_variables[initial_ptr] == 0:
+                    print('it halts')
+                # elif updated_variables[initial_ptr] < variables[initial_ptr] and ((variables[initial_ptr] - updated_variables[initial_ptr] % 2 != 0 and updated_variables[initial_ptr] % 2 != 0) or (variables[initial_ptr] - updated_variables[initial_ptr] % 2 == 0 and updated_variables[initial_ptr] % 2 == 0)):
+                #     print('halts')
+                else:
+                    raise SystemError('not halts') # give up and say that it does not halt (?)
+                    
+            variables[initial_ptr] = 0 # update variables with the initial of this cycle (or with all the changed values?)
+                                
+            """
+
 
     def __call__(self, code):
         self.analyze(code)
 
-# s = SemanticsAnalyzer()
-# #program = "+++[a=3>>+[b=1<<-][a=2]]"
+s = SemanticsAnalyzer()
+program = "+++[++[>>>[>>]]][+++++]"
 # program = "+[>++[<-]]" # a=1 b=2
-# s(program)
+s(program)
